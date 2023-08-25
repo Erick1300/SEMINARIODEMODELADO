e467a441d64fbba3bcf488dfcabc2d83ea182973
Erick1300 <142864467+Erick1300@users.noreply.github.com>
2023-08-24 20:43:29 -0600

    Add files via upload

---
+++ DIF1.m
@@ -0,0 +1,13 @@
+[t,x]=ode45(@DIF,[0 50], [0 (35*pi)/180 0 0]);
+figure(1)
+plot(t,x(:,1));
+grid on
+title("Posición de theta 1");
+xlabel("Tiempo");
+ylabel("Radianes");
+figure(2)
+plot(t,x(:,2));
+grid on
+title("Posición de theta 2");
+xlabel("Tiempo");
+ylabel("Radianes");
